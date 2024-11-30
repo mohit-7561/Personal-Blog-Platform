851e5ebcf0189038380b6945da604bfa73b0ea64
mohit-7561 <mohitsinghania666r@gmail.com>
2024-11-30 20:40:23 +0530

    fixed cors origin

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
 const postRoutes = require('./Routes/postRoutes');
 const authRoutes = require('./Routes/authRoutes');
@@ -13,7 +12,9 @@ const PORT = process.env.PORT || 5000;
 // Connect to MongoDB
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://delightful-malabi-007b4b.netlify.app',  // Allow frontend domain
+}));
 app.use(express.json());
 
 
