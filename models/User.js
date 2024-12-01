786a165533a43a2b9afce8e7bc2eb227182df223
Mohit Kumar <150506390+mohit-7561@users.noreply.github.com>
2024-12-01 15:40:04 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },      // Added 'name' field
-  email: { type: String, required: true, unique: true },  // Added 'email' field
+  name: { type: String, required: true },     
+  email: { type: String, required: true, unique: true },  
   password: { type: String, required: true },
 });
 
